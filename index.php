b58cbfb4202e2fbfb7466407dda1c4a3f2f57b57
proxza <proxza@gmail.com>
2016-12-08 16:00:09 +0200

    PHWork1:2 | Add | Select gender

--- index.php
+++ index.php
@@ -25,15 +25,19 @@
             <table>
                 <thead class="table-head">
                 <tr><td colspan="2" class="table-head-text">Форма входа</td></tr>
+                <tr><td colspan="2">&nbsp;</td></tr>
                 </thead>
                 <tbody>
                 <tr><td class="table-td-text"> Логин: </td><td><input type="text" name="username" placeholder="Введите ваше имя" maxlength="20" class="table-td-input"> </td></tr>
                 <tr><td class="table-td-text"> Пароль: </td><td><input type="password" name="userpass" placeholder="Введите ваш пароль" maxlength="15" class="table-td-input"> </td></tr>
+                <tr><td class="table-td-text"> Ваш пол: </td><td><select name="gender"><option value="0">Выберите пол</option><option value="1">Мужчина</option><option value="2">Женщина</option></select></td></tr>
+                <tr><td colspan="2">&nbsp;</td></tr>
                 <tr><td colspan="2" class="table-checkbox">Запомнить меня <input type="checkbox" value="1" name="remember"> </td></tr>
                 </tbody>
                 <tfoot class="table-foot">
                 <tr><td colspan="2"> <hr> </td></tr>
                 <tr><td colspan="2"> <input type="submit" value="Войти" class="btn"> </td></tr>
+                <tr><td colspan="2">&nbsp;</td></tr>
                 <tr><td colspan="2"><span class="little-text">Забыл пароль? | Регистрация</span></td></tr>
                 </tfoot>
             </table>
